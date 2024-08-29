fd84540ef2f6622f233a990b4c0433de92e2f373
CescDev <medicalsos068@gmail.com>
2024-08-29 16:46:35 -0600

    Agregando efecto de carusel con las marcas

---
+++ app/(routes)/(home)/components/SlidersBrands/SlidersBrands.data.ts
@@ -0,0 +1,23 @@
+export const dataBrands = [
+  {
+    url: "Chery-Logo.png",
+  },
+  {
+    url: "chevrolet.png",
+  },
+  {
+    url: "honda.png",
+  },
+  {
+    url: "hyundai.png",
+  },
+  {
+    url: "KIA.png",
+  },
+  {
+    url: "Nissan.png",
+  },
+  {
+    url: "toyota.png",
+  }
+];
--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/Shared/Navbar";
 import { FirstBlock } from "./components/FirstBlock/FirstBlock";
+import { SliderBrands } from "./components/SlidersBrands";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <div>
         <Navbar />
         <FirstBlock />
+        <SliderBrands />
       </div>
     </div>
   );
